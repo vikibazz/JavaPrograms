61074f3d88a7604b9795edf0573d5b05944b1c67
vikibazz <vickybala2810@gmail.com>
2019-09-12 14:47:56 +0530

    small change in ThreeSum.java

--- javaAlgorithms/src/threeSum/ThreeSum.java
+++ javaAlgorithms/src/threeSum/ThreeSum.java
@@ -28,9 +28,10 @@ public class ThreeSum
     static List<List<Integer>> res = new LinkedList<>();
     static List<List<Integer>> listOfThreeSum(int [] num)
     {
+        //Sorting the array
         Arrays.sort(num);
 
-        for(int i = 0 ; i < num.length;i++)
+        for(int i = 0 ; i < num.length - 2;i++)
         {
             if(i == 0 || num[i] != num[i - 1])
             {
