013c3b27ac64bd2e168261fd8dd1dc6d9b9c78e3
vikibazz <vickybala2810@gmail.com>
2019-10-09 12:08:38 +0530

    just a simple change

---
+++ javaAlgorithms/src/simpleDictionary/Dictionary.java
@@ -0,0 +1,10 @@
+public class Dictionary
+{
+    public static void main(String[] args) 
+    {
+        char Puzzle [][] = {{'a','z','o','l'},{'n','x','h','o'},{'v','y','i','v'},{'o','r','s','e'}};
+        String Dictionary[] = {"van","love","zoho","is","are"};
+
+        //Not finished
+    }
+}
