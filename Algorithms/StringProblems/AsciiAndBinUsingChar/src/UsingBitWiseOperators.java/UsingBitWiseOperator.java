44fa11d6ee7447167de1f7690155422c4fcd32f4
vikibazz <vickybala2810@gmail.com>
2019-12-16 14:27:49 +0530

    Adding new file in AsciiAndBinUsingChar called UsingBitWiseOperator.java

---
+++ Algorithms/StringProblems/AsciiAndBinUsingChar/src/UsingBitWiseOperators.java/UsingBitWiseOperator.java
@@ -0,0 +1,30 @@
+public class UsingBitWiseOperator
+{
+    public static void binaryValueIs(char c,int Ascii) 
+    {
+        int k = 0;
+        System.out.println("The Binary value of "+c+" is :");
+        for(int i = 31;i >= 0;i--)
+        {
+            k = Ascii >> i;
+
+            if((k & 1) > 0)
+            {
+                System.out.print("1");
+            }
+            else
+            {
+                System.out.print("0");
+            }
+        }
+    }
+    public static void main(String[] args) 
+    {
+        char c = 'A';
+        
+        int Ascii = c;
+        System.out.println("Ascii value of : "+c+" is : \n"+Ascii);
+
+        binaryValueIs(c,Ascii);
+    }
+}
